2c2be10532822224a8b7dd02dae4e438a35c82e3
Sabrina LeMay <b33na1997@gmail.com>
2022-09-14 20:48:45 -0700

    added dashboard routes

---
+++ controllers/dashBoardRoutes.js
@@ -0,0 +1,115 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, Comment, User } = require('../models');
+const auth = require('../utils/auth');
+const { post } = require('./api');
+
+router.get('/', auth, (req,res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'content_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'github']
+            }
+        ]
+    }).then(postDataDB => {
+        const posts = postDataDB.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', auth, (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'github']
+            }
+        ]
+    }).then(postDataDB => {
+        if (!postDataDB) {
+            res.status(404).json({ message: 'No post with this id was found' });
+            return;
+        }
+        const post = postDataDB.get({ plain: true });
+        res.render('edit-post', {
+            post, loggedIn: true
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/create/', auth, (req,res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            },
+            {
+            model: User,
+            attributes: ['username', 'github']
+            }
+        ]
+    }).then(postDataDB => {
+        const posts = postDataDB.map(post => post.get({ plain: true }));
+        res.render('create-post', { posts, loggedIn: true });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
