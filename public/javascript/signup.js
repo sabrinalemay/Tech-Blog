8cff265d997616c0abc139aec62217bfc45606eb
Sabrina LeMay <b33na1997@gmail.com>
2022-09-25 13:45:40 -0700

    added signup.js to javascript

---
+++ public/javascript/signup.js
@@ -0,0 +1,28 @@
+async function signup(event) {
+    event.preventDefault();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('password-signup').value.trim();
+    const github = document.querySelector('#github-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                email,
+                github,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log('succesful');
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signup);
