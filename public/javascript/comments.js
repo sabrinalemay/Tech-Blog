7ee9ec7edac8a7ef542ae7a836fd8015278b510c
Sabrina LeMay <b33na1997@gmail.com>
2022-09-25 12:42:00 -0700

    added comment.js to javascript

---
+++ public/javascript/comments.js
@@ -0,0 +1,27 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+    const commentText = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const postId = window.location.toString().split('/')[
+        window.location.toString().split('/').length-1
+    ];
+
+    if (commentText) {
+        const respone = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                postId,
+                commentText
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (respone.ok) {
+            document.location.reload();
+        } else {
+            alert(respone.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
