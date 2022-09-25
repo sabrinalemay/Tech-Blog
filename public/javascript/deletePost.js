eaaa5ad4ded498cd860cc186f6f26c7317a4f0d9
Sabrina LeMay <b33na1997@gmail.com>
2022-09-25 12:51:28 -0700

    added deletePost to javascript

---
+++ public/javascript/deletePost.js
@@ -0,0 +1,26 @@
+const { header } = require("express/lib/request");
+const res = require("express/lib/response");
+
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length-1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
